4805d7b27f412913ed9abafe3801b7e370d1ce8b
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-02-01 16:14:44 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const mongoose=require('mongoose');
 const route=require('./Route/routes');
 const dotenv =require('dotenv').config()
 const {PORT,MONGOOSE_STRING}=process.env;
+const cors=require('cors');
 const app=express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 mongoose.connect(MONGOOSE_STRING,{usenewurlparser:true})
 .then(()=>console.log('connected to mongoose'))
